9dd999fe2c5cc75c31778b8d80c5fcbc0ee95f6b
Lijo Jolly <lijojolly10@gmail.com>
2024-03-22 00:20:04 +0530

    Add files via upload

---
+++ Positive Or Negative.c
@@ -0,0 +1,17 @@
+//To find the number is positive or negative
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("\nEnter a Number: ");
+    scanf("%d",&num);
+    if (num >0)
+    {
+        printf("\nThe number is positive",num);
+    }
+
+    else (num <0);
+    {
+        printf("\nThe number is negative",num);
+    }
+}
